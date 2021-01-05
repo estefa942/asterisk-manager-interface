2e85cff874d7f3c99a03926c3bd99f62dd3b724b
Andrés Isaza <andres.isaza1506@gmail.com>
2021-01-05 15:21:04 -0500

    added .gitignore, deleted node_modules

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 const express = require("express");
 const asterikAmi = require("asterisk-ami");
 const app = express();
-const port = 3000
+const port = 3000;
 
-const ami = new asterikAmi({host:'192.168.30.71', username:'admin',password:'root'})
-
-ami.on('ami_data', function(data){
-	console.log('AMI DATA', data);
-	//decide between Events and non events here and what to do with them, maybe run an event emitter for the ones you care about
+const ami = new asterikAmi({
+  host: "192.168.30.71",
+  username: "admin",
+  password: "root",
 });
 
-ami.connect(function(){
-	ami.send({action: 'Ping'});//run a callback event when we have connected to the socket
-});//connect creates a socket connection and sends the login action
+ami.on("ami_data", function (data) {
+  console.log("AMI DATA", data);
+  //decide between Events and non events here and what to do with them, maybe run an event emitter for the ones you care about
+});
 
-ami.send({action: 'Ping'});
+ami.connect(function () {
+  ami.send({ action: "Ping" }); //run a callback event when we have connected to the socket
+}); //connect creates a socket connection and sends the login action
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+ami.send({ action: "Ping" });
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
