8cb989a17bc4f252b803a9b89620348af3fb361b
Andrés Isaza <andres.isaza1506@gmail.com>
2021-01-23 19:31:31 -0500

    prueba de ami finalizada

--- server.js
+++ server.js
@@ -2,22 +2,26 @@ const AmiClient = require("asterisk-ami-client");
 let client = new AmiClient();
 
 client
-  .connect("admin", "root", { host: "192.168.30.71", port: 5038 })
+  .connect("admin", "root", { host: "192.168.10.26", port: 5038 })
   .then((amiConnection) => {
     client
-      .on("connect", () => console.log("connect"))
-      .on("event", (event) => console.log(event))
-      .on("data", (chunk) => console.log(chunk))
-      .on("response", (response) => console.log(response))
-      .on("disconnect", () => console.log("disconnect"))
-      .on("reconnection", () => console.log("reconnection"))
-      .on("internalError", (error) => console.log(error))
+      .on("ExtensionStatus", (event) => {
+        switch (event.Status) {
+          case "1":
+            console.log(`Extensión ${event.Exten} en uso`);
+            break;
+          case "0":
+            console.log(`Extensión ${event.Exten} libre`);
+          default:
+            break;
+        }
+      })
+      .on("event", console.log)
+      .on("Hangup", console.log)
+      .on("connect", console.log)
+      .on("response", console.log)
       .action({
         Action: "Ping",
       });
-
-    setTimeout(() => {
-      client.disconnect();
-    }, 5000);
   })
   .catch((error) => console.log(error));
